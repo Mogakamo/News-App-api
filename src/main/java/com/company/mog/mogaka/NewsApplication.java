03ade8a9803977f8c6a50de19a0c282ef211f1c1
Mogakamo <mogaka.amo254@gmail.com>
2021-07-03 21:20:50 +0300

    synced the project and got to create the main app

---
+++ src/main/java/com/company/mog/mogaka/NewsApplication.java
@@ -0,0 +1,7 @@
+package com.company.mog.mogaka;
+
+public class NewsApplication {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/mogaka/NewsApp.java
+++
@@ -1,4 +0,0 @@
-package mogaka;
-
-public class NewsApp {
-}
