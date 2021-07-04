be33f1006d826322d34c95987968872a6ee667ab
Mogakamo <mogaka.amo254@gmail.com>
2021-07-04 14:49:24 +0300

    finished working on the get api part

--- src/main/java/com/company/mog/mogaka/NewsApplication.java
+++ src/main/java/com/company/mog/mogaka/NewsApplication.java
@@ -12,30 +12,26 @@ import java.net.URLEncoder;
 
 public class NewsApplication {
     public static void main(String[] args) throws Exception {
-//        //Host URL
-//        String host = "https://google-news1.p.rapidapi.com";
-//        String charset = "UTF-8";
-//        //Headers for the request
-//        String x_rapidapi_host = "google-news1.p.rapidapi.com";
-//        String x_rapidapi_key = "63a618be9cmsh26eab68acd52dc5p10";//rapid api key
-//        //Params
-//        String s = "Pulp";
-//        //Format query to avoid encoding problems
-//        String query = String.format("s=%s", URLEncoder.encode(s, charset));
-
         HttpResponse<JsonNode> response = Unirest.get("https://google-news1.p.rapidapi.com/topic-headlines?topic=WORLD&country=US&lang=en")
                 .header("x-rapidapi-key", "cb50711466msh8ab234bdc5f2765p1e59a1jsnd0b9f945cd64")
                 .header("x-rapidapi-host", "google-news1.p.rapidapi.com")
                 .asJson();
 
+        HttpResponse<JsonNode> response1 = Unirest.get("https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=HtjYdnd2cGXa9bNpf0XA4uEzrt9hnzBQ")
+                .header("new-york_timesapi-key", "HtjYdnd2cGXa9bNpf0XA4uEzrt9hnzBQ")
+                .header("newyork_times", "api.nytimes.com")
+                .asJson();
+
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         JsonParser jp = new JsonParser();
         JsonElement je = jp.parse(response.getBody().toString());
+        JsonElement je1 = jp.parse(response1.getBody().toString());
         String prettyJsonString = gson.toJson(je);
+        String prettyJsonString1 = gson.toJson(je1);
 
+        System.out.println("Google News ...\n");
         System.out.println(prettyJsonString);
-
-//        System.out.println(response.getStatus());
-//        System.out.println(response.getHeaders().get("Content-Type"));
+        System.out.println("New York Times ... \n");
+        System.out.println(prettyJsonString1);
     }
 }
