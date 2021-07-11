7082f1a1ae46e2a91c4f4560fcba007e2fff2ce3
danomwa <danomwaopiyo@gmail.com>
2021-07-11 14:37:44 +0300

    removed the whole code

---
+++ src/main/java/com/news/nyt/NewsApplication.java
@@ -0,0 +1,17 @@
+package com.news.nyt;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
+import java.net.URLEncoder;
+
+public class NewsApplication {
+    public static void main(String[] args) {
+
+    }
+}
