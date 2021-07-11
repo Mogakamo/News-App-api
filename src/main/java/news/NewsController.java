b5cfa2ac620cea9d5277640056b8ef076892996d
danomwa <danomwaopiyo@gmail.com>
2021-07-11 19:05:54 +0300

    finished the first controller and also got to add the swagger ui

--- src/main/java/news/NewsController.java
+++ src/main/java/news/NewsController.java
@@ -1,4 +1,21 @@
 package news;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class NewsController {
+
+    @Autowired
+    private NewsService service;
+
+    @SuppressWarnings("rawtypes")
+    @GetMapping("/getTopStories")
+    public Map getTopStories() {
+        return service.getTopStories();
+    }
 }
